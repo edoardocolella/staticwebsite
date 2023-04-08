29ddc30dfddef662af9811ae4fb13ed846f4f75a
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-04-08 14:42:52 +0200

    new projects session

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -10,4 +9,14 @@ const observer = new IntersectionObserver((entries) => {
 })
 
 const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) => {observer.observe(el)})
+hiddenElements.forEach((el) => { observer.observe(el) })
+
+const divSnap = document.getElementById('containSnap');
+const progress = document.getElementById('progressbar');
+
+divSnap.onscroll = function () {
+
+    let totalHeight = divSnap.scrollHeight - window.innerHeight;
+    let progressHeight = (divSnap.scrollTop / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
